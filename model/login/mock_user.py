f2be223394271ce7569e095c8025745905904531
Pedro Rodrigues Teixeira <pedro51teixeira@gmail.com>
2025-06-12 10:19:16 -0300

    Atualização da documentação

---
+++ model/login/mock_user.py
@@ -0,0 +1,11 @@
+
+class MockDatabase:
+    def __init__(self):
+        pass
+        """
+        
+        Esse arquivo servirá de "banco de dados" para inicializarmos os usuários.
+        Nessa classe estarão algumas informações do usuário, como email, senha...
+        Todos esses dados serão pré-definidos por nós. 
+        
+        """
--- model/mock_user.py
+++
@@ -1,4 +0,0 @@
-
-class MockDatabase:
-    def __init__(self):
-        pass #Aqui deverão ser colocados os dados para o cadastro do usuário
